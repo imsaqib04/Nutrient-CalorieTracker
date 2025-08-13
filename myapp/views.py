7c167409de9c39385d552f7a20a13b80fdd9e4e4
saqib <saqibansari2011@gmail.com>
2025-08-13 23:36:56 +0530

    add features

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
-
+from.models import Food,Consume
 # Create your views here.
 
+# def index(request):
+#     if request.method=="POST":
+#         food_consumed = request.POST['food_consumed'] # this give me only not obj
+#         consume_obj = Food.objects.get(name=food_consumed) # this give obj
+#         user = request.user
+#         consume = Consume(user=user,food_consumed=consume)
+#         consume.save()
+#         foods =  Food.objects.all()
+#     else:
+#         foods =  Food.objects.all()
+#     consumed_food=Consume.objects.filter(user=request.user)
+#     return render(request,"myapp/index.html",{
+#         'foods':foods,
+#         'consumed_food':consumed_food
+#     })
+
+
 def index(request):
-    return render(request,"myapp/index.html")
+    if request.method == "POST":
+        food_consumed = request.POST['food_consumed']
+        consume_obj = Food.objects.get(name=food_consumed)
+        user = request.user
+        Consume.objects.create(user=user, food_consumed=consume_obj)
+
+    foods = Food.objects.all()
+
+    if request.user.is_authenticated:
+        consumed_food = Consume.objects.filter(user=request.user)
+    else:
+        consumed_food = []
+
+    return render(request, "myapp/index.html", {
+        'foods': foods,
+        'consumed_food': consumed_food
+    })
+
+
